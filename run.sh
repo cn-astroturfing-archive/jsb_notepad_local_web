6e2f2f57f7454ff8d5cff112bfda4851f8dac697
cn-astroturfing-archive <247458571+cn-astroturfing-archive@users.noreply.github.com>
2026-01-28 23:28:07 -0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python3 ~/download_notebookvip_assets.py \
-  --runtime-url "https://jsb.notebookvip.cn/jsb-wap/static/js/runtime.2c059b5cf33ac3cb1752.js" \
+  --runtime-url "https://jsb.notebookvip.cn/jsb-wap/static/js/runtime.d6390d3ff74ff4e0029d.js" \
   --out jsb \
   --public-path "/jsb-wap/"
